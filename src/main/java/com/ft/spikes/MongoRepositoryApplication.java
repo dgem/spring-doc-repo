fe6e4b69c483c81e34d4df8ac307ae7b82cf4f3f
Murphy <dan.murphy@ft.com>
2015-11-30 17:46:04 +0000

    Remove autowired repository in MongoRepositoryApplication, see MongoRepositoryApplicationTests

--- src/main/java/com/ft/spikes/MongoRepositoryApplication.java
+++ src/main/java/com/ft/spikes/MongoRepositoryApplication.java
@@ -1,7 +1,5 @@
 package com.ft.spikes;
 
-import com.ft.spikes.repositories.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongoRepositoryApplication implements CommandLineRunner
 {
-    @Autowired
-    public PersonRepository people;
-
     public static void main(String[] args) {
         SpringApplication.run(MongoRepositoryApplication.class, args);
     }
